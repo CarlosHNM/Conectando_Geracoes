49e8b978e0fb80e8e50266f1a5f8a0bc75a72e80
Carlos Henrique <31441314+CarlosHNM@users.noreply.github.com>
2024-11-23 01:06:10 -0300

    Primeiro Commit GitHub

---
+++ ConectandoGeracoes/Controllers/CursoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConectandoGeracoes.Controllers
+{
+    public class CursoController : Controller
+    {
+        public IActionResult CursoSmartphone()
+        {
+            return View();
+        }
+
+        public IActionResult CursoTablet()
+        {
+            return View();
+        }
+
+        public IActionResult CursoComputador()
+        {
+            return View();
+        }
+        public IActionResult CursoComprasOnline()
+        {
+            return View();
+        }
+        public IActionResult CursoInternetBaking()
+        {
+            return View();
+        }
+        public IActionResult ServicoSaude()
+        {
+            return View();
+        }
+    }
+}
